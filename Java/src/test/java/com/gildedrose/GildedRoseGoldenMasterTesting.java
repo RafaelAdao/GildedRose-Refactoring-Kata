f5ac8037c1f22b036ea5447ad1bcfa975b93b3bd
kewerson hugo <kewerson.hugo@gmail.com>
2017-07-20 03:27:14 -0300

    Adicionado uma alternativa ao golden master testing que compara linha por linha

--- Java/src/test/java/com/gildedrose/GildedRoseGoldenMasterTesting.java
+++ Java/src/test/java/com/gildedrose/GildedRoseGoldenMasterTesting.java
@@ -2,6 +2,7 @@ package com.gildedrose;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
@@ -20,6 +21,9 @@ public class GildedRoseGoldenMasterTesting {
 
 	public static final Integer FIXED_SEED = 1;
 	public static final Integer NUMBER_OF_ITEMS = 5000;
+	public static final String MASTER_SAMPLE_FILE_NAME = "goldenMasterSample.txt";
+	public static final String CURRENT_SAMPLE_FILE_NAME = "currentSample.txt";
+
 	public static final Integer MAX_SELLIN = 10;
 	public static final Integer MIN_SELLIN = -10;
 	public static final Integer MAX_QUALITY = 50;
@@ -32,18 +36,40 @@ public class GildedRoseGoldenMasterTesting {
 
 	@Test
 	public void generateGoldenMaster() throws IOException {
-		Path outputSamepleFilePath = Paths.get("currentSample.txt");
 		Item[] randomItems = generateRandomItems(NUMBER_OF_ITEMS);
 		GildedRose gildedRoseApp = new GildedRose(randomItems);
 		gildedRoseApp.updateQuality();
-		createOutputFile(outputSamepleFilePath, randomItems);
-		String currentSampleFile = openFileAsString("currentOutput.txt");
-		String goldenSampleFile = openFileAsString("goldenMasterSample.txt");
+		generateSampleFileFrom(randomItems);
+		String currentSampleFile = openFileAsString(CURRENT_SAMPLE_FILE_NAME);
+		String goldenSampleFile = openFileAsString(MASTER_SAMPLE_FILE_NAME);
 		assertEquals(currentSampleFile, goldenSampleFile);
 	}
 
-	private void createOutputFile(Path path, Item[] items) throws IOException {
-		try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+	@Test
+	public void generateGoldenMasterLineByLineAlternative() throws IOException {
+		Item[] randomItems = generateRandomItems(NUMBER_OF_ITEMS);
+		GildedRose gildedRoseApp = new GildedRose(randomItems);
+		gildedRoseApp.updateQuality();
+		generateSampleFileFrom(randomItems);
+		compareFileLineByLine();
+	}
+
+	private void compareFileLineByLine() throws IOException {
+		try (BufferedReader currentSampleReader = Files.newBufferedReader(Paths.get(CURRENT_SAMPLE_FILE_NAME));
+				BufferedReader masterSampleReader = Files.newBufferedReader(Paths.get(MASTER_SAMPLE_FILE_NAME));) {
+			while (true) {
+				String lineFromCurrentSample = currentSampleReader.readLine();
+				String lineFromMasterSample = masterSampleReader.readLine();
+				if (lineFromMasterSample == null && lineFromCurrentSample == null)
+					break;
+				assertEquals(lineFromMasterSample, lineFromCurrentSample);
+			}
+		}
+	}
+
+	private void generateSampleFileFrom(Item[] items) throws IOException {
+		Path outputSamepleFilePath = Paths.get(CURRENT_SAMPLE_FILE_NAME);
+		try (BufferedWriter writer = Files.newBufferedWriter(outputSamepleFilePath)) {
 			for (Item item : items) {
 				writer.write(item.toString() + "\n");
 			}
@@ -67,6 +93,7 @@ public class GildedRoseGoldenMasterTesting {
 	private Item randomItem() {
 		return new Item(randomName(), randomSellIn(), randomQuality());
 	}
+
 	private String randomName() {
 		return ITEMS_NAME[0 + random.nextInt(ITEMS_NAME.length)];
 	}
@@ -74,7 +101,7 @@ public class GildedRoseGoldenMasterTesting {
 	private int randomSellIn() {
 		return generateRandomNumberBetween(MIN_SELLIN, MAX_SELLIN);
 	}
-	
+
 	private int randomQuality() {
 		return generateRandomNumberBetween(MIN_QUALITY, MAX_QUALITY);
 	}
